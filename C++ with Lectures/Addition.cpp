01a46ecacb4357cb559f57a78768b41e5b4f0301
takshiljain9-shah <Takshiljain.shah@gmail.com>
2026-01-21 17:35:24 +0530

    Create Addition.cpp

---
+++ C++ with Lectures/Addition.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x, y;
+
+    cout << "Enter the value of X and y. ";
+    cin >> x >> y;
+
+    int z = x + y;
+
+    cout << "The z is " << z;
+
+    return 0;
+}
