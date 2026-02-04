59224c6d1ab04c7f487527c40239eb4280624dea
takshiljain9-shah <Takshiljain.shah@gmail.com>
2026-02-04 20:31:10 +0530

    Create MatrixProgram1.cpp

---
+++ Classroom C++/MatrixProgram1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    int r, c;
+    int x[10][10], y[10][10], sum [10][10];
+    
+    cout << "Enter values for rows and columns";
+    cin >> r >> c;
+
+    cout << "Enter values for first matrix";
+    for(int i = 0; i <= r; i++){
+        for (int j = 0; j <= c; j++){
+            cin >> x[i][j];
+        }
+        cout << " "; 
+        for 
+        if [i][j]
+    }
+    
+}
