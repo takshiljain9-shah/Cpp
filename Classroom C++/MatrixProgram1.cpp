b305cc2168e08816c5da2d98414f951816d44240
takshiljain9-shah <Takshiljain.shah@gmail.com>
2026-02-05 14:09:17 +0530

    Update MatrixProgram1.cpp

--- Classroom C++/MatrixProgram1.cpp
+++ Classroom C++/MatrixProgram1.cpp
@@ -2,21 +2,38 @@
 
 using namespace std;
 
-int main (){
+int main () {
     int r, c;
-    int x[10][10], y[10][10], sum [10][10];
+    int x[10][10], y[10][10], sum[10][10];
     
-    cout << "Enter values for rows and columns";
+    cout << "Enter values for rows and columns (max 10): ";
     cin >> r >> c;
 
-    cout << "Enter values for first matrix";
-    for(int i = 0; i <= r; i++){
-        for (int j = 0; j <= c; j++){
+    // 1. Input for First Matrix
+    cout << "Enter values for first matrix: " << endl;
+    for(int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
             cin >> x[i][j];
         }
-        cout << " "; 
-        for 
-        if [i][j]
     }
-    
+
+    // 2. Input for Second Matrix
+    cout << "Enter values for second matrix: " << endl;
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            cin >> y[i][j];
+        }
+    }
+
+    // 3. Calculate Sum and Display Result
+    cout << "The sum of the two matrices is: " << endl;
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            sum[i][j] = x[i][j] + y[i][j]; // Calculate
+            cout << sum[i][j] << " ";      // Print element
+        }
+        cout << endl; // New line after each row
+    }
+
+    return 0;
 }
