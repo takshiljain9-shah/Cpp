40b2c4abd368f52b064196c8e53af19c9625544f
takshiljain9-shah <Takshiljain.shah@gmail.com>
2026-02-08 18:45:54 +0530

    Create Array.cpp

---
+++ Classroom C++/Array.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int i;
+    int arr[5] ={1,3,5,7,9};
+    
+    for(i=0;i<=4;i++){
+        cout<< arr[i] << " odd number: " << endl;
+    }
+    return 0;
+}
