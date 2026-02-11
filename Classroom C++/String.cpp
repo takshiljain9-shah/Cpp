b3376b98ea2c1efdeb8a45f8e88a31a11f271a83
takshiljain9-shah <Takshiljain.shah@gmail.com>
2026-02-11 12:56:43 +0530

    Update String.cpp

--- Classroom C++/String.cpp
+++ Classroom C++/String.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(){
-    string
-    string x = ""
+    string message = "Let's suppose you have to print a big message and use it again and again in a code, you can call the message fucntion again and again in the code.";
+    cout << message;
+
+    return 0;
 }
