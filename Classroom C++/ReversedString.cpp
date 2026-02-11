666b43fe7d5b299f5a34257a2c9bcd9aca8eee0a
takshiljain9-shah <Takshiljain.shah@gmail.com>
2026-02-11 12:56:54 +0530

    Create ReversedString.cpp

---
+++ Classroom C++/ReversedString.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string str;
+
+    cout << "Enter a string: ";
+    cin >> str;
+
+    string rev = "";
+
+    for (int i = str.length() - 1; i >= 0; i--){
+        rev += str[i];
+    }
+    cout << "The reversed string is : " << rev;
+    
+    return 0;
+}
