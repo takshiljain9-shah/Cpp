0e3d3f527615dbe6786e066ab99e94479976bf03
takshiljain9-shah <Takshiljain.shah@gmail.com>
2026-02-12 14:37:08 +0530

    Create HelloLearners.cpp

---
+++ C++ Full Course/1 Basics/HelloLearners.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello, Learners this is the most basic code which i write in every language:)";
+    return 0;
+}
+
+/*This 
+is
+ how
+  we 
+  can
+   write
+    multi
+       line 
+            x=comments
+:)
+*/
