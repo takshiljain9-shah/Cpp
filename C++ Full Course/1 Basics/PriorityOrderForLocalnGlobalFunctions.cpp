3292da84a324b2c788a5a6e97e8b7b33796dd301
takshiljain9-shah <Takshiljain.shah@gmail.com>
2026-02-12 14:37:04 +0530

    Create PriorityOrderForLocalnGlobalFunctions.cpp

---
+++ C++ Full Course/1 Basics/PriorityOrderForLocalnGlobalFunctions.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int glo = 9; //An global variable is declared, it is outside of the main funciton.
+
+void sum(){
+    cout << glo; //Prints the global function, because there is no local function declared inside void funciton.
+}
+
+int main(){
+    int glo = 18; //Local variable declared inside main function
+    sum(); // Using the sum function here
+    cout << glo; //It will print local function because it is defined inside main function
+
+    return 0;
+}
