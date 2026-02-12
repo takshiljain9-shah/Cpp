add32657a2c5c114c437c37e9c5f755a7728c978
takshiljain9-shah <Takshiljain.shah@gmail.com>
2026-02-12 14:37:01 +0530

    Create TakingUsersInput.cpp

---
+++ C++ Full Course/1 Basics/TakingUsersInput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a;
+
+    cout << "Enter a number: "; //Insertion 
+    cin >> a;// Extraction
+
+    cout << a;
+
+    return 0;
+}
