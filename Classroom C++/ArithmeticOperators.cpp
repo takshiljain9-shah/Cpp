e44a20b0262631f41381d6ec75283c44a7de530c
takshiljain9-shah <Takshiljain.shah@gmail.com>
2026-02-18 00:21:53 +0530

    Create ArithmeticOperators.cpp

---
+++ Classroom C++/ArithmeticOperators.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a = 10, b = 20;
+
+    cout << "Addition: " << a + b << endl;
+    cout << "Subtraction: " << a - b << endl;
+    cout << "Multiplication: " << a * b << endl;
+    cout << "Division: " << a / b << endl;
+    cout << "Modulus: " << a % b << endl;
+
+    return 0;
+}
