2fd024f30964a305d30971d30ebcf2a9a0fbb32f
takshiljain9-shah <Takshiljain.shah@gmail.com>
2026-03-03 16:07:44 +0530

    Update Arrays.cpp

--- C++ Full Course/2 Functions/Arrays.cpp
+++ C++ Full Course/2 Functions/Arrays.cpp
@@ -3,6 +3,11 @@ using namespace std;
 
 int main(){
     int marks[4] = {23, 45, 56, 89};
+    int mathMarks[4];
+
+    mathMarks[0] = 4389;
+    mathMarks[1] = 13224;
+    
 
     cout << marks[0] << endl;
     cout << marks[1] << endl;
