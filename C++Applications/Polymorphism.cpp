b9673cdf8f020f38f788a586327dc4a870f4fa79
takshiljain9-shah <Takshiljain.shah@gmail.com>
2026-03-10 12:55:51 +0530

    Create Polymorphism.cpp

---
+++ C++Applications/Polymorphism.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class shape{
+    public:
+    virtual void draw(){
+        cout << "Drawing a shape: " << endl;
+    }
+};
+
+class circle : public shape{
+    public: 
+    void draw(){
+        cout << "Drawing a circle. " << endl;
+    }
+};
+
+class rectangle : public shape{
+    public:
+    void draw(){
+        cout << "Drawing a rectangle. " << endl;
+    }
+};
+
+int main(){
+    shape *s;
+
+    circle c;
+    rectangle r;
+
+    s = &c;
+    s->draw();
+
+    s = &r;
+    s->draw();
+}
