a95ad05c44074eb79e0765aa257365aa1ce0e3dd
takshiljain9-shah <Takshiljain.shah@gmail.com>
2026-03-10 14:30:02 +0530

    Create STLusingVector.cpp

---
+++ C++Applications/STLusingVector.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    vector<int> numbers;
+
+    numbers.push_back(10);
+    numbers.push_back(20);
+    numbers.push_back(30);
+
+    cout << "Vector elements: ";
+    for(int i = 0; i < numbers.size(); i++){
+        cout << numbers[i] << " ";
+    }
+    return 0;
+}
